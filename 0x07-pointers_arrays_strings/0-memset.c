c0a8b1309aed2555607670d946f20cf79c62b07c
Markberyl <musangaberyl@gmail.com>
2022-07-18 15:04:09 -0700

    more pointers

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,17 @@
 #include "main.h"
-
 /**
- * _memset - it fills a block of memory with constant byte.
- * @s: a pointer to a memory area.
- * @b:the constant byte.
- * @n: the number of bytes of memory to be filled with b.
- *
- * Return: s.
+ * _memset - fills memory with a constant byte,
+ * @s: memory area.
+ * @b: constant byte.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		*(s+i) = b;
+		*(s + i) =  b;
+
 	return (s);
 }
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,17 @@
 #include "main.h"
-
 /**
- * _memcpy - copies n bytes from memory area "src" to memory area "dest"
- * @dest: the memory area destination.
- * @src: the memory area source.
- * @n: the number of bytes to copy from "src"
- * Return: dest.
+ * _memcpy - copies memory area,
+ * @dest: destination memory area.
+ * @src: source memory area.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-		dest[i] = src[i];
+		*(dest + i) =  *(src + i);
+
 	return (dest);
 }
